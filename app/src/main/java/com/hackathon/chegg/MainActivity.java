d450cdd53d3d2dda585092890426d2eb933eca88
Rajkamal <rajkamal.singh1011@gmail.com>
2022-09-22 02:42:26 +0530

    Authentication Section front end done (login , signup fragments) used Viewpager for this.

--- app/src/main/java/com/hackathon/chegg/MainActivity.java
+++ app/src/main/java/com/hackathon/chegg/MainActivity.java
@@ -1,8 +1,12 @@
 package com.hackathon.chegg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.hackathon.chegg.Adapters.FragmentAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
 }
