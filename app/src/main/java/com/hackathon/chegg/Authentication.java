6996ee6dc48fdc5c71f7c60a5212cda4f1168067
Anii232002 <you@example.com>
2022-09-22 16:35:54 +0530

    Bottom Nav done

--- app/src/main/java/com/hackathon/chegg/Authentication.java
+++ app/src/main/java/com/hackathon/chegg/Authentication.java
@@ -3,8 +3,11 @@ package com.hackathon.chegg;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.hackathon.chegg.Adapters.FragmentAdapter;
 
@@ -14,7 +17,7 @@ public class Authentication extends AppCompatActivity {
     private static final String TAG = Authentication.class.getSimpleName();
     private ViewPager viewPager;
     private TabLayout tabLayout;
-
+    FloatingActionButton gbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +30,17 @@ public class Authentication extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
+        gbutton = findViewById(R.id.google_auth_button);
+        gbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(Authentication.this,MainActivity.class));
+
+            }
+        });
     }
+
 
 
 }
--- app/src/main/java/com/hackathon/chegg/MainActivity.java
+++ app/src/main/java/com/hackathon/chegg/MainActivity.java
@@ -1,14 +1,54 @@
 package com.hackathon.chegg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.hackathon.chegg.Fragments.Analysis_Fragment;
+import com.hackathon.chegg.Fragments.Profile_Fragment;
+import com.hackathon.chegg.Fragments.SignUpFragment;
+import com.hackathon.chegg.Fragments.Study_Fragment;
+import com.hackathon.chegg.Fragments.Test_Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Study_Fragment()).commit();
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Analysis:
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Analysis_Fragment()).commit();
+                        break;
+                    case R.id.Profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Profile_Fragment()).commit();
+                        break;
+                    case R.id.Study:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Study_Fragment()).commit();
+                        break;
+                    case R.id.Test:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Test_Fragment()).commit();
+                        break;
+                    default:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Study_Fragment()).commit();
+
+                }
+                return true;
+            }
+        });
     }
 }
