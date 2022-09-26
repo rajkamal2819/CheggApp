f095bbbec8d7dbe325aeb93b1608b54205def0ed
Anii232002 <you@example.com>
2022-09-26 12:54:33 +0530

    Wikipedia Api content fetch + specific model AR linked

--- app/src/main/java/com/hackathon/chegg/InfoDataClass.java
+++ app/src/main/java/com/hackathon/chegg/InfoDataClass.java
@@ -1,14 +1,18 @@
 package com.hackathon.chegg;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class InfoDataClass {
 
@@ -24,24 +28,31 @@ public class InfoDataClass {
     }
 
     public void getInfo(String name, VolleyResponseListener volleyResponseListener){
-        String URL = name;
-        String info="";
+        String URL = "https://en.wikipedia.org/w/api.php?action=query&exlimit=1&explaintext=1&exsentences=10&formatversion=2&prop=extracts&titles="+name+"&format=json";
+        final String[] info = {""};
 
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, URL,null,
-                new Response.Listener<JSONArray>() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL,null,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONArray response) {
+                    public void onResponse(JSONObject response) {
 
 
 
 
-                        volleyResponseListener.onResponse(info);
+                        try {
+
+                           Log.d("response1",response.getString("title"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        volleyResponseListener.onResponse(response.toString());
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
 
-
+                        Log.d("Response",error.getMessage());
 
                         volleyResponseListener.onError("No info");
                     }
