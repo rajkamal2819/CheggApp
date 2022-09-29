4aa072d51e03549bbcb40a16a32d34330db604d8
Rajkamal <rajkamal.singh1011@gmail.com>
2022-09-29 08:52:55 +0530

    Created LineChart

--- app/src/main/java/com/hackathon/chegg/Fragments/Analysis_Fragment.java
+++ app/src/main/java/com/hackathon/chegg/Fragments/Analysis_Fragment.java
@@ -8,59 +8,65 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.hackathon.chegg.R;
+import com.hackathon.chegg.databinding.FragmentAnalysisBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Analysis_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Analysis_Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Analysis_Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Analysis_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Analysis_Fragment newInstance(String param1, String param2) {
-        Analysis_Fragment fragment = new Analysis_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FragmentAnalysisBinding binding;
+    LineChart mpLineChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analysis_, container, false);
+        binding = FragmentAnalysisBinding.inflate(getLayoutInflater());
+
+        mpLineChart = binding.chart;
+
+        LineDataSet lineDataSet1 = new LineDataSet(dataValues1(),"Data Set 1");
+        LineDataSet lineDataSet2 = new LineDataSet(dataValues2(),"Data Set 2");
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet1);
+        dataSets.add(lineDataSet2);
+
+        LineData data = new LineData(dataSets);
+        mpLineChart.setData(data);
+        mpLineChart.invalidate();
+
+        return binding.getRoot();
     }
+
+    private ArrayList<Entry> dataValues1(){
+        ArrayList<Entry> dataVals = new ArrayList<>();
+        dataVals.add(new Entry(0,20));
+        dataVals.add(new Entry(1,22));
+        dataVals.add(new Entry(2,2));
+        dataVals.add(new Entry(3,10));
+        return dataVals;
+    }
+
+    private ArrayList<Entry> dataValues2(){
+        ArrayList<Entry> dataVals = new ArrayList<>();
+        dataVals.add(new Entry(0,12));
+        dataVals.add(new Entry(2,16));
+        dataVals.add(new Entry(3,23));
+        dataVals.add(new Entry(5,1));
+        dataVals.add(new Entry(7,10));
+        return dataVals;
+    }
+
+
 }
